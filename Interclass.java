fd74d27f5c04957d763f3657af7a15b6159f370a
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-12 14:59:35 +0530

    Add files via upload

---
+++ Interclass.java
@@ -0,0 +1,32 @@
+//We can define class in interface
+ interface Library {
+    void issueBook(Book a);
+    void retrieveBook(Book a);
+    public class Book {
+       int bookId;
+       String bookName;
+       int issueDate;
+       int returnDate;
+    }
+ }
+ public class Interclass implements Library {
+    public void issueBook(Book a) {
+       System.out.println("Book Issued");
+    }
+    public void retrieveBook(Book a) {
+       System.out.println("Book Retrieved");
+    }
+    public static void main(String args[]) {
+       Interclass obj = new Interclass();
+       obj.issueBook(new Library.Book());
+       obj.retrieveBook(new Library.Book());
+    }
+ }
+
+
+
+
+
+
+
+
