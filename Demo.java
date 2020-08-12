af6fd39fd9194e0336abd065a0776515c6d8a627
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-12 12:44:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,29 @@
+//TRYING 2 inherit propertises of 2 classes
+    public void foo(){
+      System.out.println("Concrete method of parent class");
+    }
+    abstract public void disp2();  
+
+ }
+ abstract class Hema{
+     abstract public void disp3();
+     public void foo(){
+         System.out.println("I can take output");
+     }
+ }
+ 
+ class Demo extends MyClass,Hema{
+    /* Must Override this method while extending
+     * MyClas
+     */
+    public void disp2()
+    {
+        System.out.println("overriding abstract method");
+    }
+    public static void main(String args[]){
+        Demo obj = new Demo();
+        obj.foo();
+        obj.disp2();
+    }
+ }
+    
