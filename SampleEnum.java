b8d70e639d80916a1be72e675fa0da3ef477b025
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-13 16:05:32 +0530

    Add files via upload

---
+++ SampleEnum.java
@@ -0,0 +1,13 @@
+// enum is class which holds constants . It provies compile time safety//
+public class SampleEnum
+{
+enum Naturalhair
+{
+	WHITE,GREY,BROWN
+}
+public static void main(String args[])
+{
+	Naturalhair a=Naturalhair.WHITE;
+	System.out.println(a);
+}
+}
