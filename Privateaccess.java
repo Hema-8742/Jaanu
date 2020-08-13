b3687032646622358fcdf5e3c8802b12f04a44d1
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-13 15:26:26 +0530

    Add files via upload

---
+++ Privateaccess.java
@@ -0,0 +1,11 @@
+class Data {
+    private String name;
+}
+
+public class Privateaccess {
+    public static void main(String[] args[])
+    {
+        Data d=new Data();
+        d.name = "Programiz";
+    }
+} 
