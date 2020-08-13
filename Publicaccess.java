ef0a100ce83e9c883c532e2036bc3ddca9e7900f
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-13 15:56:52 +0530

    Add files via upload

---
+++ Publicaccess.java
@@ -0,0 +1,11 @@
+class Data {
+    public String name;
+}
+
+public class Publicaccess {
+    public static void main(String[] args[])
+    {
+        Data d=new Data();
+        d.name = "not possible";
+    }
+}
