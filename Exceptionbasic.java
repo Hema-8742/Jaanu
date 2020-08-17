597a84a21b12c0160f09f275b1b40b36e8c03c2f
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-17 12:58:13 +0530

    Add files via upload

---
+++ Exceptionbasic.java
@@ -0,0 +1,9 @@
+class Exceptionbasic
+{ 
+    public static void main(String args[]) 
+    { 
+        int a = 30, b = 0; 
+        int c = a/b;  // cannot divide by zero 
+        System.out.println ("Result = " + c);
+    } 
+}
