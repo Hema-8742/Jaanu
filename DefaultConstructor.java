125e2462cb4ffa1f26a19d734e8dc7fad15272b4
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-19 14:21:21 +0530

    Add files via upload

---
+++ DefaultConstructor.java
@@ -0,0 +1,11 @@
+public class Defaultconstructor
+{
+    public Defaultconstructor()
+    {
+        System.out.println("This is a default constructor");
+    }
+    public static void main(String[] args)
+    {
+        Defaultconstructor smudge = new Defaultconstructor();
+    }
+ }
