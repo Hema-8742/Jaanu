f30a852dd02bacf2b6f6ab5a308f7c38c071b5f5
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-30 21:35:09 +0530

    Add files via upload

--- Abstractclass.java
+++ Abstractclass.java
@@ -1,20 +1,30 @@
-//creating one concrete and one abstract method and running it
-abstract class Mainclass {
-	abstract public void firstMethod();
-	public void secondMethod()
-	{
-		System.out.println("Concrete methods code");
+import java.util.*;
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle(){
+		return title;
 	}
+	
 }
-class Abstractclass extends Mainclass
-{
-	public void firstMethod()
-	{
-		System.out.println("Abstract methods code");
+
+class MyBook extends Book{
+    
+     void setTitle(String s)
+    {
+       title=s;
+    }
+}
+public class Abstractclass{
+	
+	public static void main(String []args){
+		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();
+		
 	}
-public static void main(String args[])
-{
-	Abstractclass je=new Abstractclass();
-	je.firstMethod();
-}
 }
