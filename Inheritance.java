065a99f95397dd691ff59920ed40e964b55d2cbd
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-30 21:36:12 +0530

    Add files via upload

--- Inheritance.java
+++ Inheritance.java
@@ -1,23 +1,28 @@
-class TestInheritence
-{
-    void meth1()
-    {
-        System.out.println("You should know i belong to base class");
-    }  
-}  
-class MainInheritance extends TestInheritence
-{
-    void meth2()
-    {
-        System.out.println("You should know i belong to child class");
-    }  
-}
-class Inheritance
-{  
-    public static void main(String args[])
-    {
-        MainInheritance d=new MainInheritance();  
-        d.meth1();  
-        d.meth2();  
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+class Arithmetic{
+    public int add(int x,int y){
+    return x+y;
     }
+}
+class Adder extends Arithmetic{
+
+}
+
+
+
+public class Inheritance{
+    public static void main(String []args){
+        // Create a new Adder object
+        Adder a = new Adder();
+        
+        // Print the name of the superclass on a new line
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
+        
+        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
+        System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
+     }
 }
