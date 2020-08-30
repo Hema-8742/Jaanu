108519ac8a062bebffba4ae002d411f3f320dd62
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-08-30 22:55:58 +0530

    Add files via upload

---
+++ LocalDateEX.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+public class LocalDateEX {
+
+	public static void main(String[] args) {
+		
+		//Current Date
+		LocalDate today = LocalDate.now();
+		System.out.println("Current Date="+today);
+    }
+}
