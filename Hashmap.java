888667260e2fd608a9b15596c3a253303183363d
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-10-10 15:37:31 +0530

    Add files via upload

---
+++ Hashmap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+class Hashmap{ 
+  public static void main(String[] args) {
+     HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+     hmap.put(1, "HEMA");
+     hmap.put(2, "RED");
+     hmap.put(3, "COLOR");
+     hmap.put(4, "DONE");
+     System.out.println("HashMap contains: "+hmap);
+  }
+}
