ed2a998b87130c04dc9bec831575b4cc55e60257
Hema-8742 <69457930+Hema-8742@users.noreply.github.com>
2020-10-24 16:36:00 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,27 @@
+package com.request;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class Add extends HttpServlet{
+	
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
+{
+	int i=Integer.parseInt(req.getParameter("n1"));
+	int j=Integer.parseInt(req.getParameter("n2"));
+	int k=i+j;
+	req.setAttribute("k", k);
+	RequestDispatcher rd=req.getRequestDispatcher("square");
+			rd.forward(req,res);
+}
+
+}
